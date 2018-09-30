f6b436f8aa5a00c149e6e52dfb6d79ce2821c8c1
mide42 <demi_05@arcor.de>
2018-09-30 18:20:40 +0200

    Anpassungen fuer AndroidStudio 3.1.4 und Bereitstellung auf GitHub.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Android-App "RelativeLayout-Demo"
+
+Android app to demonstrate how the [RelativeLayout](https://developer.android.com/reference/android/widget/RelativeLayout) works.
+
+<br>
+
+App contains several layout files, which can be "activated" by commenting in/out
+the corresponding statements in file
+[MainActivity.java](app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java).
+
+<br>
+
+Identifiers (names for classes, variables and methods), UI texts and (JavaDoc) comments are in German only.
+
+<br>
+
+----
+## Screenshots
+
+![Screenshot 1](screenshot_1.png)
+
+<br>
+
+----
+# License
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License).
---
+++ app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
@@ -0,0 +1,43 @@
+package de.mide.relativelayoutdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * App demonstriert Funktionsweise und Möglichkeiten mit RelativeLayout.
+ * Die App enthält mehrere Layout-Dateien, die in der "onCreate()"-Methode
+ * entsprechend einzukommentieren sind (immer nur eine auf einmal!)
+ * <br><br>
+ *
+ * This project is licensed under the terms of the BSD 3-Clause License.
+ */
+public class MainActivity extends Activity {
+
+    /**
+     * Bitte genau eine "setContentView()"-Anweisung einkommentieren!
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView( R.layout.with_height_kombinationen );
+
+        //setContentView( R.layout.attribut_gravity );
+
+        //setContentView( R.layout.align_parent_attribute );
+
+        //setContentView( R.layout.geschwister_bezug );
+
+        //setContentView( R.layout.geschwister_align );
+
+        //setContentView( R.layout.ui_designer );
+
+        setContentView( R.layout.margin );
+
+        //setContentView( R.layout.forward_deklaration );
+
+        //setContentView( R.layout.ueberlappen );
+    }
+
+}
