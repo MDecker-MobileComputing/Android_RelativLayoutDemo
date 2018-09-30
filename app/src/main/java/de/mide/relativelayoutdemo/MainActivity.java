e15af9e52735df1ecf0bf3181bf2d9301349c5be
mide42 <demi_05@arcor.de>
2018-09-30 18:32:35 +0200

    Tippfehler in Layoutdatei ("with" statt "width") gefixt

--- app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
+++ app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView( R.layout.with_height_kombinationen );
+        //setContentView( R.layout.width_height_kombinationen );
 
         //setContentView( R.layout.attribut_gravity );
 
