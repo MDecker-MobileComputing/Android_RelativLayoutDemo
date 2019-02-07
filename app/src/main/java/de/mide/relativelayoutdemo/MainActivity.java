2f6b223511c1982acbc734d73e2f12a891b412bf
MDe@Mingw@A8 <mde@gmx.de>
2019-02-07 20:49:11 +0100

    Upgrade auf Android Studio 3.3

--- app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
+++ app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView( R.layout.width_height_kombinationen );
+        setContentView( R.layout.width_height_kombinationen );
 
         //setContentView( R.layout.attribut_gravity );
 
@@ -33,7 +33,7 @@ public class MainActivity extends Activity {
 
         //setContentView( R.layout.ui_designer );
 
-        setContentView( R.layout.margin );
+        //setContentView( R.layout.margin );
 
         //setContentView( R.layout.forward_deklaration );
 
