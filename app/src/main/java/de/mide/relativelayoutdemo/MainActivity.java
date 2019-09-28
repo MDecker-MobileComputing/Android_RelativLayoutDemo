42cfe2bb5fbe1757b8f171f57d80cdcc8ddd0fa8
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-09-28 22:52:09 +0200

    Upgrade auf Android Studio 3.5

--- app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
+++ app/src/main/java/de/mide/relativelayoutdemo/MainActivity.java
@@ -15,13 +15,16 @@ import android.os.Bundle;
 public class MainActivity extends Activity {
 
     /**
+     * Lifecycle-Methode, lädt (nur) Layout-Datei.
+     *
      * Bitte genau eine "setContentView()"-Anweisung einkommentieren!
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
-        setContentView( R.layout.width_height_kombinationen );
+        //setContentView( R.layout.width_height_kombinationen );
 
         //setContentView( R.layout.attribut_gravity );
 
@@ -33,7 +36,7 @@ public class MainActivity extends Activity {
 
         //setContentView( R.layout.ui_designer );
 
-        //setContentView( R.layout.margin );
+        setContentView( R.layout.margin );
 
         //setContentView( R.layout.forward_deklaration );
 
